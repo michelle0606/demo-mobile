b58df42aee2061d1239bf0087da9b66d1dd43776
walle <michelle210181@gmail.com>
2019-06-29 16:11:30 +0800

    demo

---
+++ main.js
@@ -0,0 +1,34 @@
+const endpoint =
+  'https://emma.pixnet.cc/mainpage/blog/categories/hot/28?api_version=2'
+const data = []
+
+fetch(endpoint)
+  .then(blob => blob.json())
+  .then(response => {
+    data.push(...response.articles)
+    displayData(data)
+  })
+
+const suggestion = document.querySelector('.suggestion')
+const button = document.querySelectorAll('button')
+
+function displayData(data) {
+  data.forEach(article => {
+    if (article.cover !== '') {
+      suggestion.innerHTML += `
+        <a href="${article.link}" class="list" target="_blank">
+          <img class="cover"  src='${article.cover}'>
+          <div>${article.title}</div>
+        </a>
+      `
+    }
+  })
+}
+
+function search() {
+  console.log(this.id)
+}
+
+button.forEach(b => {
+  b.addEventListener('click', search)
+})
